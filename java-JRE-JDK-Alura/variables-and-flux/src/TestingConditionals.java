a6b021c073fb2485c28aa5dd7c9ce63682b2b047
Nicole Calderari <nicole_calderari@hotmail.com>
2022-03-05 12:33:44 -0300

    Alura Class: While

--- java-JRE-JDK-Alura/variables-and-flux/src/TestingConditionals.java
+++ java-JRE-JDK-Alura/variables-and-flux/src/TestingConditionals.java
@@ -1,15 +1,13 @@
 
 public class TestingConditionals {
-  public static void main(String[] args) {
-	  int age = 22;
-	  if (age >= 18 ) {
-		  
-	  } else {
-		 
-		  System.out.println("your are still a teenager");
-	  }
-	  
-	  
-	  
-}
+	public static void main(String[] args) {
+		int age = 22;
+		if (age >= 18) {
+
+		} else {
+
+			System.out.println("your are still a teenager");
+		}
+
+	}
 }
