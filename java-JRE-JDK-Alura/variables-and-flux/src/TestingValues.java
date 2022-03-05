4fed851ae49977637bed59ec3d6250bf2a349c55
Nicole Calderari <nicole_calderari@hotmail.com>
2022-03-05 10:53:16 -0300

    Alura Class: How variables store data value

---
+++ java-JRE-JDK-Alura/variables-and-flux/src/TestingValues.java
@@ -0,0 +1,11 @@
+
+public class TestingValues {
+  public static void main(String[] args) {
+	int first = 6;
+	int second = 18; 
+	second = first;
+	first = 3;
+	System.out.println(second);
+			
+}
+}
