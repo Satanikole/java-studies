b579ff1fe7f8758dd1bf7d38747e41a75a90c273
Nicole Calderari <nicole_calderari@hotmail.com>
2022-03-05 10:44:55 -0300

    Alura Class: Char and String

---
+++ java-JRE-JDK-Alura/variables-and-flux/src/CharTesting.java
@@ -0,0 +1,17 @@
+
+public class CharTesting {
+   public static void main (String[] args) {
+	   char letter = 'n';
+	   System.out.println(letter);
+	   
+	   char value = 77;
+	   System.out.println(value);
+	   
+	   value = (char) (value + 3);
+	   System.out.println(value);
+	   
+	   String word = "I miss javascript so much";
+	   System.out.println(word);
+	   
+   }
+}
