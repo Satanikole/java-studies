9b7c6c184322e62b9a963e893eba1d07652a780a
Nicole Calderari <nicole_calderari@hotmail.com>
2022-03-05 12:11:48 -0300

    Alura Class: Bolean conditionals

---
+++ java-JRE-JDK-Alura/variables-and-flux/src/TestingConditionalsBooleans.java
@@ -0,0 +1,17 @@
+
+public class TestingConditionalsBooleans {
+	 public static void main(String[] args) {
+		  int age = 22;
+		  boolean companions = true;
+		  
+		  if (age >= 18  || companions ) {
+			  System.out.println("welcome");
+		  } else {
+			 
+			  System.out.println("you can't go in");
+		  }
+		  
+		  
+		  
+	}
+}
