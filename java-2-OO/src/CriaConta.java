c1b9df8dc37995c0cdeba2239460d8a6b0374592
Nicole Calderari <nicole_calderari@hotmail.com>
2022-06-27 16:44:48 -0300

    aula 2

---
+++ java-2-OO/src/CriaConta.java
@@ -0,0 +1,7 @@
+public class CriaConta {
+  public static void main(String[] args) {
+    Conta primeiraConta = new Conta();
+    primeiraConta.saldo = 20.10; 
+    System.out.println(primeiraConta.saldo);
+  }
+}
