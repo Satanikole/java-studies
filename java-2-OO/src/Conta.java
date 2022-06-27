79cc57251ff5cafb46d9ae89c6ea054516b9d554
Nicole Calderari <nicole_calderari@hotmail.com>
2022-06-27 17:26:19 -0300

    sexta aula

--- java-2-OO/src/Conta.java
+++ java-2-OO/src/Conta.java
@@ -5,7 +5,16 @@ class Conta {
   String titular;
   
   public void depositar(double valor ) {
-    saldo = saldo + valor;
+    saldo += valor;
+  }
+
+  public boolean saca(double valor) {
+     if(saldo >= valor) {
+      saldo -= valor;
+      return true;
+     } else {
+      return false;
+     }
   }
 
 }
--- java-2-OO/src/CriaConta.java
+++ java-2-OO/src/CriaConta.java
@@ -1,11 +1,14 @@
 public class CriaConta {
   public static void main(String[] args) {
     Conta primeiraConta = new Conta();
-    primeiraConta.saldo = 20.10; 
+    primeiraConta.saldo = 200.10; 
     System.out.println(primeiraConta.saldo);
 
     Conta segundaConta = new Conta();
     segundaConta.saldo = 80;
     System.out.println(segundaConta.titular);
+
+    primeiraConta.saca(10.0);
+    System.out.println(primeiraConta.saldo);
   }
 }
